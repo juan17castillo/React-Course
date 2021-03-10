a6bb61e584a8aeb89e06a0e8904de4a5bf30a0f6
juan17castillo <juancastillo9910@gmail.com>
2021-03-09 19:58:27 -0500

    console errors fixed and key person added

--- working-with-lists-conditionals/src/App.js
+++ working-with-lists-conditionals/src/App.js
@@ -21,13 +21,17 @@ class App extends Component {
   };
 
   nameChangedHandler = (event, id) => {
-    this.setState({
-      persons: [
-        {name: "Max", age: 28 },
-        {name: event.target.value, age: 29 },
-        {name: "Stephany", age: 26 },
-      ],
+    const personIndex = this.state.persons.findIndex((p) => {
+      return p.id === id;
     });
+    const person = {
+      ...this.state.persons[personIndex],
+    };
+    person.name = event.target.value;
+    const persons = [...this.state.persons];
+    persons[personIndex] = person;
+
+    this.setState({ persons: persons });
   };
 
   togglePersonsHandler = () => {
@@ -54,7 +58,7 @@ class App extends Component {
               <Person
                 click={() => this.deletePersonHandler(index)}
                 name={person.name}
-                age={person.name}
+                age={person.age}
                 key={person.id}
                 changed={(event) => this.nameChangedHandler(event, person.id)}
               />
