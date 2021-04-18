370abdb319ce5a80cad2a7837bd82f7215a7ec44
Juan Manuel Castillo <juancastillo9910@gmail.com>
2021-04-18 08:53:29 -0500

    useState and props assignmnet added

---
+++ first-assignment/src/components/Expenses/Expenses.js
@@ -0,0 +1,45 @@
+import React, { useState } from "react";
+
+import ExpenseItem from "./ExpenseItem";
+import ExpensesFilter from "./ExpensesFilter";
+import Card from "../UI/Card";
+import "./Expenses.css";
+
+const Expenses = (props) => {
+  const [filteredYear, setfilteredYear] = useState("2020");
+
+  const changeDropdownSelected = (selectedYear) => {
+    setfilteredYear(selectedYear);
+  };
+
+  return (
+    <Card className="expenses">
+      <ExpensesFilter
+        selectedValue={filteredYear}
+        onChangeFilter={changeDropdownSelected}
+      />
+      <ExpenseItem
+        title={props.items[0].title}
+        amount={props.items[0].amount}
+        date={props.items[0].date}
+      />
+      <ExpenseItem
+        title={props.items[1].title}
+        amount={props.items[1].amount}
+        date={props.items[1].date}
+      />
+      <ExpenseItem
+        title={props.items[2].title}
+        amount={props.items[2].amount}
+        date={props.items[2].date}
+      />
+      <ExpenseItem
+        title={props.items[3].title}
+        amount={props.items[3].amount}
+        date={props.items[3].date}
+      />
+    </Card>
+  );
+};
+
+export default Expenses;
