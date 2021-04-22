9077f46ab03e8e4b9bc307e78a9a82b94b21325e
Juan Manuel Castillo <juancastillo9910@gmail.com>
2021-04-22 14:49:32 -0500

    Dynamic style properties and chatbar added

--- List and conditional content/src/components/Expenses/Expenses.js
+++ List and conditional content/src/components/Expenses/Expenses.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 
 import ExpensesList from "./ExpensesList";
 import ExpensesFilter from "./ExpensesFilter";
+import ExpensesChart from "./ExpensesChart";
 import Card from "../UI/Card";
 import "./Expenses.css";
 
@@ -16,14 +17,13 @@ const Expenses = (props) => {
     return expense.date.getFullYear().toString() === filteredYear;
   });
 
-  
-
   return (
     <Card className="expenses">
       <ExpensesFilter
         selectedValue={filteredYear}
         onChangeFilter={changeDropdownSelected}
       />
+      <ExpensesChart expenses={filteredExpenses} />
       <ExpensesList items={filteredExpenses} />
     </Card>
   );
