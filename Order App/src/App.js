ffcbb652240c6d41f03dd2f3e3a21a5b621c58c1
Juan Manuel Castillo <juancastillo9910@gmail.com>
2021-05-03 11:52:27 -0500

    Managing state items in a global context

--- Order App/src/App.js
+++ Order App/src/App.js
@@ -1,17 +1,28 @@
-import { Fragment } from "react";
+import { useState } from "react";
+import CartProvider from "./store/CartProvider";
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
 import Cart from "./components/Cart/Cart";
 
 function App() {
+  const [cartIsShown, setCartIsShown] = useState(false);
+
+  const showCartHandler = () => {
+    setCartIsShown(true);
+  };
+
+  const hideCartHandler = () => {
+    setCartIsShown(false);
+  };
+
   return (
-    <Fragment>
-      <Cart />
-      <Header />
+    <CartProvider>
+      {cartIsShown && <Cart onClose={hideCartHandler} />}
+      <Header onShowCart={showCartHandler} />
       <main>
         <Meals />
       </main>
-    </Fragment>
+    </CartProvider>
   );
 }
 
