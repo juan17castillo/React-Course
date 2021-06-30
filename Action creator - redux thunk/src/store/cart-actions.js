7f196a2139e8a8359a9fbaa1b3d6cc438cc6157e
Juan Manuel Castillo <juancastillo9910@gmail.com>
2021-06-29 19:28:53 -0500

    Deleting firebase urls

--- Action creator - redux thunk/src/store/cart-actions.js
+++ Action creator - redux thunk/src/store/cart-actions.js
@@ -5,7 +5,7 @@ export const fetchCartData = () => {
   return async (dispatch) => {
     const fetchData = async () => {
       const response = await fetch(
-        "https://react-e6848-default-rtdb.firebaseio.com/cart.json"
+        ""
       );
 
       if (!response.ok) {
@@ -49,7 +49,7 @@ export const sendCartData = (cart) => {
 
     const sendRequest = async () => {
       const response = await fetch(
-        "https://react-e6848-default-rtdb.firebaseio.com/cart.json",
+        "",
         {
           method: "PUT",
           body: JSON.stringify({
