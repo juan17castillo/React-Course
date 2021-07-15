f2ffc290ca6df1dcbbc3d81b1ed218fb6adc158e
Juan Manuel Castillo <juancastillo9910@gmail.com>
2021-07-15 15:21:42 -0500

    First steps: Generics, Union and inference types

---
+++ React Typescript/with-typescript.js
@@ -0,0 +1,5 @@
+var addNumbers = function (a, b) {
+    return a + b;
+};
+var c = addNumbers(2, 2);
+console.log(c);
---
+++ React Typescript/with-typescript.ts
@@ -0,0 +1,7 @@
+const addNumbers = (a: number, b: number) => {
+    return a + b;
+}
+
+const c: number = addNumbers(2,2)
+
+console.log(c);
